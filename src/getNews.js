59c5c185516b540f36571f68e103acf8c315d486
Jung-Jaeho <wjdrudgh123@naver.com>
2020-10-12 22:23:40 +0900

    time plus

--- src/getNews.js
+++ src/getNews.js
@@ -49,7 +49,7 @@ export const getNews = async (url) => {
         "//div[@id='boxContents']/div[@style='']/div/div[@class='box_contents']/div/ul/li"
       )
     ),
-    15000
+    30000
   );
   for (let i = 0; i < newsList.length; i++) {
     const anchors = await newsList[i].findElements(By.xpath("./span/a"));
