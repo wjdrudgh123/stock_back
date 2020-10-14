490c664cb635f42367060c5d771aa06dc88637a1
Jung-Jaeho <wjdrudgh123@naver.com>
2020-10-15 00:50:46 +0900

    try catch

--- src/scrapper.js
+++ src/scrapper.js
@@ -164,35 +164,39 @@ const getNews = async (driver) => {
   );
   await pageTap.click();
   // 페이지 이동 후 로딩때문에
-  const newsList = await driver.wait(
-    until.elementsLocated(
-      By.xpath(
-        "//div[@id='boxContents']/div[@style='']/div/div[@class='box_contents']/div/ul/li"
-      )
-    ),
-    150000
-  );
-  for (let i = 0; i < newsList.length; i++) {
-    const anchors = await newsList[i].findElements(By.xpath("./span/a"));
-    const p = await newsList[i].findElement(
-      By.xpath("./span/p[@class='date']")
+  try {
+    const newsList = await driver.wait(
+      until.elementsLocated(
+        By.xpath(
+          "//div[@id='boxContents']/div[@style='']/div/div[@class='box_contents']/div/ul/li"
+        )
+      ),
+      50000
     );
-    const tmpDate = await p.getText();
-    const splitDate = await tmpDate.split("·");
-    const trimDate = splitDate[1].trim();
-    const splitYear = trimDate.split(".");
-    const monthDate = Number(`${splitYear[1]}${splitYear[2]}`);
-    if (numStockDate <= monthDate) {
-      const title = await anchors[0].getText();
-      const description = await anchors[1].getText();
-      const link = await anchors[1].getAttribute("href");
-      const newsData = { title: title, description: description, link: link };
-      news.push(newsData);
+    for (let i = 0; i < newsList.length; i++) {
+      const anchors = await newsList[i].findElements(By.xpath("./span/a"));
+      const p = await newsList[i].findElement(
+        By.xpath("./span/p[@class='date']")
+      );
+      const tmpDate = await p.getText();
+      const splitDate = await tmpDate.split("·");
+      const trimDate = splitDate[1].trim();
+      const splitYear = trimDate.split(".");
+      const monthDate = Number(`${splitYear[1]}${splitYear[2]}`);
+      if (numStockDate <= monthDate) {
+        const title = await anchors[0].getText();
+        const description = await anchors[1].getText();
+        const link = await anchors[1].getAttribute("href");
+        const newsData = { title: title, description: description, link: link };
+        news.push(newsData);
+      }
     }
+  } catch (err) {
+    console.loe(`err: ${err}`);
+  } finally {
+    console.log("end getNews");
+    return news;
   }
-
-  console.log("end getNews");
-  return news;
 };
 
 /*
