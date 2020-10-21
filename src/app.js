be8a89ef744e4773f4a3b9000bb6403b3c4ca5e9
Jung-Jaeho <wjdrudgh123@naver.com>
2020-10-21 23:21:55 +0900

    get company

--- src/app.js
+++ src/app.js
@@ -1,82 +1,39 @@
 import express from "express";
-import { getGoldenCrossCompany, companyInfo, getNews } from "./scrapper";
+import { startSearching } from "./stockChecker";
 import cors from "cors";
 import schedule from "node-schedule";
 
 const PORT = 3000;
 
 const app = express();
-let GOLDENCROSS_LIST = [];
-let CONMPANY_INFO = [];
-let NEWS = [];
+
 let TODAY_DATA = [];
+let initFlag = false; // 처음 서버 실행할때
 
-const getGoldenCross = async () => {
-  const d = new Date();
-  const date = `${d.getFullYear()}.${d.getMonth() + 1}.${d.getDate()}`;
-  console.log(`Time: ${date} Get Golden Cross Start`);
-  GOLDENCROSS_LIST = await getGoldenCrossCompany();
-  console.log(`Time: ${date} Get Golden Cross End`);
-  await getAutoData();
+const getCompany = async () => {
+  TODAY_DATA = startSearching();
 };
 
-const getAutoData = async () => {
-  console.log(`Get Company Info Start`);
-  CONMPANY_INFO = await companyInfo(GOLDENCROSS_LIST);
-  console.log(`Get Company Info End`);
-};
-
-const getAutoNews = async () => {
-  console.log("Get New Start");
-  NEWS = await getNews(GOLDENCROSS_LIST);
-  console.log("Get New End");
-};
-const getGolendCrossJob1 = schedule.scheduleJob(
-  "0 00 16 * * 1-5",
-  getGoldenCross
-);
-const getGolendCrossJob2 = schedule.scheduleJob(
-  "0 30 16 * * 1-5",
-  getGoldenCross
-);
-
-const getCompaniesJob1 = schedule.scheduleJob("0 00 17 * * *", getAutoNews);
-const getCompaniesJob2 = schedule.scheduleJob("0 00 19 * * *", getAutoNews);
-const getCompaniesJob3 = schedule.scheduleJob("0 00 21 * * *", getAutoNews);
-const getCompaniesJob4 = schedule.scheduleJob("0 00 23 * * *", getAutoNews);
+const getCompaniesJob = schedule.scheduleJob("00 05 18 * * 1-5", getCompany);
 
 const corsOptions = {
   origin: ["http://localhost:3000", "https://recomstock.netlify.app"],
-  //origin: "https://recomstock.netlify.app",
   credentials: true,
 };
 
 app.use(cors(corsOptions));
 
+const getCompany = async () => {
+  TODAY_DATA = startSearching();
+};
+
 const returnJson = async (req, res) => {
-  console.log("start scrap");
-  console.log(`TODAY GOLDEN ITEM: ${GOLDENCROSS_LIST}`);
-  // res.header("Access-Control-Allow-Origin", "https://recomstock.netlify.app");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
-  // res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  // res.header("Access-Control-Allow-Credentials", true);
-  if (TODAY_DATA.length === 0) {
-    console.log(`${TODAY_DATA.length}/ scrap start cause no data`);
-    TODAY_DATA.push("Init");
-    await getGoldenCross();
-    await getAutoNews();
+  console.log("GET /data START");
+
+  if (TODAY_DATA.length === 0 && initFlag === false) {
+    getCompany();
+    initFlag = true;
   }
-  console.log("end scrap");
-  if (TODAY_DATA[0] === "Init") {
-    TODAY_DATA.splice(0, 1);
-  }
-  for (let index = 0; index < CONMPANY_INFO.length; index++) {
-    CONMPANY_INFO[index]["news"] = NEWS[index];
-  }
-  TODAY_DATA = CONMPANY_INFO;
   console.log("start send json");
   res.json(TODAY_DATA);
   console.log("end send json");
---
+++ src/stockChecker.js
@@ -0,0 +1,218 @@
+import { Builder, By, until } from "selenium-webdriver";
+import chrome, { ServiceBuilder } from "selenium-webdriver/chrome";
+import path from "path";
+
+/*
+https://finance.naver.com/sise/sise_fall.nhn
+*/
+
+const URL_KOSPI = "https://finance.naver.com/sise/sise_fall.nhn?sosok=0";
+const URL_KOSDAQ = "https://finance.naver.com/sise/sise_fall.nhn?sosok=1";
+let FINAL_PICK = [];
+
+const init = async () => {
+  const driverPath = path.join(__dirname, "../chromedriver");
+  const serviceBuilder = new ServiceBuilder(driverPath);
+  const options = new chrome.Options();
+  options.addArguments(
+    "headless",
+    "disable-gpu",
+    "no-sandbox",
+    "disable-dev-shm-usage"
+  );
+  const driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(options)
+    .setChromeService(serviceBuilder)
+    .build();
+  return driver;
+};
+
+const connectServer = async (url) => {
+  const driver = await init();
+  console.log("Start checkFallStock");
+  await driver.get(url);
+  return driver;
+};
+
+export const startSearching = async () => {
+  await searchingSetting(URL_KOSPI);
+  await searchingSetting(URL_KOSDAQ);
+  return FINAL_PICK;
+};
+
+// 검색 조건 세팅
+const searchingSetting = async (url) => {
+  console.log("Start checkFallStock");
+  let companyNames = [];
+
+  const driver = await connectServer(url);
+  let radioBtns = await checkSettingRadioBtn(driver, "check per checked");
+
+  if (radioBtns.per === "true" && radioBtns.trade === null) {
+    await radioBtns.perRadio.click(); // per 체크해제
+    await radioBtns.tradeRadio.click(); // 전일거래량 체크
+    await (
+      await driver.findElement(
+        By.xpath("//*[@id='contentarea_left']/div[2]/form/div/div/div/a[1]")
+      )
+    ).click(); // 적용하기 클릭
+    // 다시 확인
+    radioBtns = await checkSettingRadioBtn(driver, "click radio button");
+    console.log(radioBtns.per, radioBtns.trade);
+    if (radioBtns.per === null && radioBtns.trade === "true") {
+      const trTag = await radioBtns.driver.findElements(
+        By.xpath("//*[@id='contentarea']/div[3]/table/tbody/tr")
+      );
+      for (let i = 2; i < trTag.length; i++) {
+        const companies = await getProperCompany(trTag[i]);
+        if (companies !== false) {
+          companyNames.push(companies);
+        }
+      }
+    }
+  }
+  await radioBtns.driver.quit();
+  console.log(`LIST COUNT: ${companyNames.length}`);
+  console.log(`FINISH GET PROPER COMPANY`);
+  console.log(`CHECK COMPANY START`);
+  for (let index = 0; index < companyNames.length; index++) {
+    const pickCompany = await checkCompany(companyNames[index]);
+    if (pickCompany !== false) {
+      FINAL_PICK.push(pickCompany);
+    }
+  }
+  console.log(`CHECK COMPANY FINISH`);
+  console.log(FINAL_PICK);
+};
+
+const checkSettingRadioBtn = async (driver, log) => {
+  console.log(`SETTING RADIO BUTTON: ${log}`);
+
+  const chkPerRadio = await driver.wait(
+    until.elementLocated(By.xpath("//*[@id='option6']")),
+    10000
+  );
+  const chkLastTradeRadio = await driver.wait(
+    until.elementLocated(By.xpath("//*[@id='option9']")),
+    10000
+  );
+  const chkPerRadioAttr = await chkPerRadio.getAttribute("checked"); // checked면 true 아니면 null
+  const chkLastTradeRadioAttr = await chkLastTradeRadio.getAttribute("checked"); // checked면 true 아니면 null
+
+  const returnValue = {
+    perRadio: chkPerRadio,
+    tradeRadio: chkLastTradeRadio,
+    per: chkPerRadioAttr,
+    trade: chkLastTradeRadioAttr,
+    driver: driver, // 확인할때 생긴 driver
+  };
+
+  return returnValue;
+};
+
+const getProperCompany = async (tr) => {
+  console.log(`GET PROPER COMPANY FUNC START`);
+  /*
+    거래량 10% 차이나는 종목 확인
+  */
+  const checkTr = await tr
+    .findElement(By.xpath("./td[1]"))
+    .getAttribute("class");
+  if (checkTr === "no") {
+    const price = await tr.findElement(By.xpath("./td[3]")).getText();
+    if (Number(price.replace(/\,/g, "")) > 1000) {
+      const lastTrade = await tr.findElement(By.xpath("./td[6]")).getText();
+      const todayTrade = await tr.findElement(By.xpath("./td[7]")).getText();
+
+      const tradeRating =
+        ((Number(todayTrade.replace(/\,/g, "")) -
+          Number(lastTrade.replace(/\,/g, ""))) /
+          Number(todayTrade.replace(/\,/g, ""))) *
+        100;
+      if (tradeRating > 20) {
+        const aTag = await tr.findElement(By.xpath("./td[2]/a"));
+        const name = await aTag.getText();
+        const url = await aTag.getAttribute("href");
+
+        if (validatorCompanyName(name)) {
+          return { name: name, url: url };
+        }
+      }
+    }
+  }
+  return false;
+};
+
+const validatorCompanyName = (name) => {
+  const lastWord = name.substring(name.length - 2, name.length);
+  if (
+    lastWord === "우B" ||
+    lastWord === "우A" ||
+    lastWord.charAt(lastWord.length - 1) === "우" ||
+    name.indexOf("KINDEX") !== -1 ||
+    name.indexOf("KODEX") !== -1 ||
+    name.indexOf("국고채") !== -1 ||
+    name.indexOf("선물") !== -1 ||
+    name.indexOf("TIGER") !== -1 ||
+    name.indexOf("호스팩") !== -1 ||
+    name.indexOf("(전환)") !== -1 ||
+    name.indexOf("3호") !== -1 ||
+    name.indexOf("ETN") !== -1
+  ) {
+    return false;
+  }
+  return true;
+};
+
+const checkCompany = async ({ name, url }) => {
+  console.log(`CHECK COMPANY FUNC`);
+  const driver = await connectServer(`${url}`);
+  await (
+    await driver.wait(
+      until.elementLocated(By.xpath("//*[@id='content']/ul/li[2]/a")),
+      10000
+    )
+  ).click(); // 뉴스탭 클릭
+  await driver.manage().setTimeouts({ implicit: 15000 });
+  const iframes = await driver.wait(
+    until.elementsLocated(By.css("iframe")),
+    10000
+  );
+  for (let i = 0; i < iframes.length; i++) {
+    const iframeTitle = await iframes[i].getAttribute("title");
+    if (iframeTitle === "일별 시세") {
+      console.log(`SWITCH FRAME`);
+      // iframe으로 스위치
+      await driver.switchTo().frame(iframes[i]);
+      const marketPrices = await driver.wait(
+        until.elementsLocated(By.xpath("/html/body/table[1]/tbody/tr")),
+        10000
+      );
+      let lowPrice = 999999999;
+      for (let j = 2; j < 7; j++) {
+        const checkPricePoll = await marketPrices[j]
+          .findElement(By.xpath("./td[3]/span"))
+          .getAttribute("class");
+        const tmpLowPrice = await (
+          await marketPrices[j].findElement(
+            By.xpath("/html/body/table[1]/tbody/tr[3]/td[6]/span")
+          )
+        ).getText();
+        const lowPric = Number(tmpLowPrice.replace(/\,/g, ""));
+
+        if (lowPric < lowPrice) {
+          lowPrice = lowPric;
+        }
+        const pollSplit = checkPricePoll.split(" ");
+
+        if (pollSplit[2] === "red02") {
+          await driver.quit();
+          return { name: name, lowPrice: lowPrice };
+        }
+      }
+      await driver.quit();
+      return false;
+    }
+  }
+};
